5e10a1ae23bbcf0342376af4372654c37baa0632
Ashish Narayan Shinde <ashishshinde765@gmail.com>
2022-03-30 14:28:49 +0530

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'CRUDPP-2.0.1';
+}
